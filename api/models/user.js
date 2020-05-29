4f4e0a4e66650b89df5506f744b5124094b99e85
RohitSi20 <64515540+RohitSi20@users.noreply.github.com>
2020-05-29 11:40:35 +0530

    Update user.js
    Password Encryption update

--- api/models/user.js
+++ api/models/user.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var Schema = mongoose.Schema,
+bcrypt = require('bcrypt'),
+SALT_WORK_FACTOR = 10;
 
 var UserSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
@@ -71,5 +73,34 @@ var UserSchema = new Schema({
   }
 
 });
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  
+  if (!user.isModified('password')) return next();
+
+  
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+
+      
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
+
 
 module.exports = mongoose.model('user', UserSchema);
